b475b21946a372536f7fa245a56ac143213377d5
Анна Емельянова <victoriaemelyanova2001@gmail.com>
2024-04-11 02:16:18 +0300

    Testing integrated sorting

--- PanarinaSortingWorkspace/PanarinaSortingWorkspace.py
+++ PanarinaSortingWorkspace/PanarinaSortingWorkspace.py
@@ -12,4 +12,19 @@ while y<1 or y>5:                                                               
     print ("1 - Быстрая сортировка \n")                                                     #
     print ("2 - Встроенная сортировка \n")                                                  #
     print ("3 - Вставкой \n")                                                               #
-    y = int(input ())                                                                            #
+    y = int(input ())                                                                       #
+mas1 = sorted(mas)
+i=0                                                                                         #
+while i<n:                                                                                  #
+    print (mas1[i], "\n")                                                                    #
+    i=i+1                                                                                   #
+i=0                                                                                            #
+
+while i<n:                                                                                  #
+    mas[i] = sorted(mas[i]) 
+    i=i+1                                                                                   #
+mas = sorted(mas)
+i=0                                                                                         #
+while i<n:                                                                                  #
+    print (mas[i], "\n")                                                                    #
+    i=i+1                                                                                   #
