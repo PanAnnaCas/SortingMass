67ef1875b96b2ea05912356da8cc249175c84b97
Анна Емельянова <victoriaemelyanova2001@gmail.com>
2024-04-10 12:21:45 +0300

    Все изменения касаются попыток объединить ветви

--- PanarinaSortingWorkspace/PanarinaSortingWorkspace.py
+++ PanarinaSortingWorkspace/PanarinaSortingWorkspace.py
@@ -1,14 +1,14 @@
-from PanarinaSortingProject import mass
-
-mas, n = mass()
-print ("\n \n Импорт данных прошел успешно. Ваш массив для сортировки выведен ниже \n" )
-i=0
-while i<n:
-    print (mas[i])
-    i=i+1
-while y<1 or y>5:
-    print ("Как мы будем сортировать массив? \n")
-    print ("1 - Быстрая сортировка \n")
-    print ("2 - Встроенная сортировка \n")
-    print ("3 - Вставкой \n")
-   
+from PanarinaSortingProject import mass                                                     #
+                                                                                            #
+mas, n = mass()                                                                             #
+print ("\n \n Импорт данных прошел успешно. Ваш массив для сортировки выведен ниже \n" )    #
+i=0                                                                                         #
+while i<n:                                                                                  #
+    print (mas[i])                                                                          #
+    i=i+1                                                                                   #
+while y<1 or y>5:                                                                           #
+    print ("Как мы будем сортировать массив? \n")                                           #
+    print ("1 - Быстрая сортировка \n")                                                     #
+    print ("2 - Встроенная сортировка \n")                                                  #
+    print ("3 - Вставкой \n")                                                               #
+                                                                                            #
