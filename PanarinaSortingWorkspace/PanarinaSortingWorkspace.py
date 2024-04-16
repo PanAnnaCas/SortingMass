b7af261113bf79b3d05083785a016b4f3e95b4c2
Анна Емельянова <victoriaemelyanova2001@gmail.com>
2024-04-16 15:43:03 +0300

    All 3 algorithms has been finished

--- PanarinaSortingWorkspace/PanarinaSortingWorkspace.py
+++ PanarinaSortingWorkspace/PanarinaSortingWorkspace.py
@@ -7,34 +7,36 @@ print ("\n \n Импорт данных прошел успешно. Ваш ма
 for mas2 in mas:                                                                           #Выводим на экран массив для сортировки
     print (*mas2)                                                                          #                                                                                #
 y=10
-print ("Как мы будем сортировать массив? \n")                                           
+print ("\n Как мы будем сортировать массив? \n")
+t1, t2, t3= 0, 0, 0
 while y:                                                                                   #Выбираем способ сортировки
     if y==0: break
     elif y==1: 
-        print ("You've chosen quiksort")
         mass, t1 = qwicksort (mas, n)
-        break
-    elif y==2: 
+        #break
+        print ("Быстрая сортировка окончена")
+    elif y==2:
         mass, t2 = sortinginteg(mas, n)
-        break
+        #break
+        print ("Встроенная сортировка окончена")
     elif y==3: 
         print ("You've chosen вставку")
         mass, t3 = insertion(mas, n)
-        break
+        #break
+        print ("Сортировка окончена")
     else:     
         print ("Выберите один из вариантов: \n")                                           
         print ("1 - Быстрая сортировка \n")                                                     
         print ("2 - Встроенная сортировка \n")                                                  
         print ("3 - Вставкой \n") 
-        print ("0 - Выйти из программы \n")
-        y = int(input ())
+        print ("0 - Выйти из программы \n") 
+    y = int(input ())
 print ("\n Отсортированный массив: \n")
 for mas2 in mass:                                                                         #Вывод отсортированного массива
-    for mas0 in mas1:    
+    for mas0 in mas2:    
         print (f'{mas0:4}', end = "|")
     print ("\n")
 
-print ("\n Время выполнения: ", t1+t2+t3)
-print ("\n Время выполнения быстрой сортировки: ", t1)
-print ("\n Время выполнения встроенной сортировки: ", t2)
-print ("\n Время выполнения сортировки вставкой: ", t3)
+print ("    Время выполнения быстрой сортировки: ", t1)
+print ("    Время выполнения встроенной сортировки: ", t2)
+print ("    Время выполнения сортировки вставкой: ", t3)
